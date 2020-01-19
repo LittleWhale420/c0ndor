80b94d74c7f1273805fac416e18e7f4060d209af
Little Whale <vladimirputin420@protonmail.com>
2020-01-19 15:24:10 +0300

    created and fetched config

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const POINT = "point"
 const PERCENTAGE = "percentage"
 const GEOLOCATION = "geolocation"
 
-function createButton(title, type){
+fetchData("config.json", (responseText) => {
+    console.log(responseText)
+})
+
+
+function createButton(title, type) {
     const button = document.createElement("button")
     button.className = type
     button.innerHTML = title
@@ -13,3 +18,19 @@ createButton("huy", PERCENTAGE);
 createButton("huy", PERCENTAGE);
 createButton("huy", PERCENTAGE);
 createButton("huy", PERCENTAGE);
+
+function fetchData(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+ 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4) return;
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+            callback(xhr.responseText)
+        }
+    }
+       xhr.send();
+}
